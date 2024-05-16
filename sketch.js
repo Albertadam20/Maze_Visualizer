1706f3e61280089550011ba50d14c9d99cd6462e
Albertadam20 <albi2300@gmail.com>
2024-05-16 12:49:54 +0200

    lavede en variabel der hedder test

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ let maze_state = "start"
 let maze_timer = 0                                            
 let time = 0                                              
 let maze_ON_OFF = "OFF"  
+let test
 let center = 160
 let adj_up = 141
 let adj_down = 0
